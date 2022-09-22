496373cd9f63b20546f45aba7f6b675937d7ad04
Savant527 <jhonsebastiannavas@gmail.com>
2022-09-22 11:10:55 -0500

    Gráficos terminados + correción en detalle

---
+++ WebPractica_SebastianNavas/WebPractica/obj/Release/net5.0/PubTmp/Out/wwwroot/js/Tablas.js
@@ -0,0 +1,8 @@
+﻿var datatable;
+$(document).ready(function () {
+    datatable = $('#tblData').DataTable({
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.12.1/i18n/es-ES.json"
+        }
+    });
+});
